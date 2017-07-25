35cacb2ea10c9b6e2f00aa2cc0be5acc212518f1
adr-007 <adr-007@ukr.net>
2017-07-25 20:09:54 +0300

    test 100500

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:$PORT runp-heroku:app
+web: python showweather/run.py
--- showweather/run.py
+++ showweather/run.py
@@ -1,4 +1,4 @@
 from showweather import app
 import os
 
-app.run(port=int(os.environ["PORT"]))
+app.run(port=int(os.environ["PORT"]) if "PORT" in os.environ else 5000)
